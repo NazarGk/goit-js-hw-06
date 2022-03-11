770b14089b6886ee8b6bac8e3014956c10f80031
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-11 21:59:40 +0200

    Task #1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,47 @@
-const categoriesEl = document.querySelector('item');
-console.log(categoriesEl);
+
+
+
+// Sorry, but i cant do that task with your navigation of DOM =/ Make my best.
+
+
+
+
+const categoriesEl = document.querySelectorAll('.item');
+console.log('Number of categories: ', categoriesEl.length);
+
+
+
+const args = [...categoriesEl];
+
+
+
+const argsOfFirstList = args[0];
+
+const categoriesTitleElFirst = argsOfFirstList.querySelector('h2');
+console.log('Category: ', categoriesTitleElFirst.textContent);
+
+const elementsOfCategoriesFirst = argsOfFirstList.querySelectorAll('li');
+console.log('Elements: ', elementsOfCategoriesFirst.length);
+
+
+
+
+const argsOfSecondList = args[1];
+
+const categoriesTitleElSecond = argsOfSecondList.querySelector('h2');
+console.log('Category: ', categoriesTitleElSecond.textContent);
+
+const elementsOfCategoriesSecond = argsOfSecondList.querySelectorAll('li');
+console.log('Elements: ', elementsOfCategoriesSecond.length);
+
+
+
+
+const argsOfThirdList = args[2];
+
+const categoriesTitleElThird = argsOfThirdList.querySelector('h2');
+console.log('Category: ', categoriesTitleElThird.textContent);
+
+const elementsOfCategoriesThird = argsOfThirdList.querySelectorAll('li');
+console.log('Elements: ', elementsOfCategoriesThird.length);
+
