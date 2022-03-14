01de5349bd574a3f63efa500a6e6e2484d053b07
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-14 15:34:52 +0200

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,23 @@
+
+
+const counterValue = document.querySelector('#value');
+
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+
+
+buttonIncrement.addEventListener('click', onTargetButtonClickIncrement);
+buttonDecrement.addEventListener('click', onTargetButtonClickDecrement);
+
+
+let number = 0;
+
+function onTargetButtonClickIncrement(){
+   number++;
+   counterValue.textContent = number;
+}
+
+function onTargetButtonClickDecrement(){
+    number--;
+    counterValue.textContent = number;
+}
