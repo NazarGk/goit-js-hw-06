eb770502a92ced885adfde07f57be992e095ca76
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-16 15:10:44 +0200

    Task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const buttonChangeEl = document.querySelector('.change-color');
+const inSpanColorEl = document.querySelector('.color');
+
+buttonChangeEl.addEventListener('click', changeBodyColor);
+
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  inSpanColorEl.textContent = document.body.style.backgroundColor;
 }
