12797100d60a900cd36d326155e7fe4ceab906fb
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-16 14:29:28 +0200

    Task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
 
+
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', handleSumbit);
+
+
+function handleSumbit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+    
+    formData.forEach((name, value) => {
+        console.log(value, name);
+    });
+
+    if(event.currentTarget.elements.email.value == '' || event.currentTarget.elements.password.value == ''){
+        return alert('Use your keybord man!');
+    }
+    event.currentTarget.reset();
+}
