c88ae28144c882390f16dac5167cdfc5ddc724b3
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-17 10:59:01 +0200

    Task 9 Update

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -5,7 +5,13 @@ const inSpanColorEl = document.querySelector('.color');
 buttonChangeEl.addEventListener('click', changeBodyColor);
 
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 function changeBodyColor() {
-  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  document.body.style.backgroundColor = getRandomHexColor();
   inSpanColorEl.textContent = document.body.style.backgroundColor;
 }
