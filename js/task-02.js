e8ec80bb901db356957c754a3a4edee95b6c141e
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-21 15:38:50 +0200

    Task 2 Updated

--- js/task-02.js
+++ js/task-02.js
@@ -8,48 +8,39 @@ const ingredients = [
 ];
 
 
-console.log(ingredients);
 
 const firstListEl = document.createElement('li');
 firstListEl.textContent = ingredients[0];
 firstListEl.classList = 'item';
-
-console.log(firstListEl);
 
 
 const secondListEl = document.createElement('li');
 secondListEl.textContent = ingredients[1];
 secondListEl.classList = 'item';
 
-console.log(secondListEl);
 
 
 const thirdListEl = document.createElement('li');
 thirdListEl.textContent = ingredients[2];
 thirdListEl.classList = 'item';
 
-console.log(thirdListEl);
 
 
 const fourthListEl = document.createElement('li');
 fourthListEl.textContent = ingredients[3];
 fourthListEl.classList = 'item';
 
-console.log(fourthListEl);
 
 
 const fifthListEl = document.createElement('li');
 fifthListEl.textContent = ingredients[4];
 fifthListEl.classList = 'item';
 
-console.log(fifthListEl);
 
 
 const sixthListEl = document.createElement('li');
 sixthListEl.textContent = ingredients[5];
 sixthListEl.classList = 'item';
-
-console.log(sixthListEl);
 
 
 const ingredientsUlEl = document.querySelector('#ingredients');
