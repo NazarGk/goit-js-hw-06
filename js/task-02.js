64ca9fdfdc51e5c82f28c44b35e1f7ae45a4426d
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-22 17:08:07 +0200

    Task 2 Update NEW

--- js/task-02.js
+++ js/task-02.js
@@ -7,43 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
-
-
-const firstListEl = document.createElement('li');
-firstListEl.textContent = ingredients[0];
-firstListEl.classList = 'item';
-
-
-const secondListEl = document.createElement('li');
-secondListEl.textContent = ingredients[1];
-secondListEl.classList = 'item';
-
-
-
-const thirdListEl = document.createElement('li');
-thirdListEl.textContent = ingredients[2];
-thirdListEl.classList = 'item';
-
-
-
-const fourthListEl = document.createElement('li');
-fourthListEl.textContent = ingredients[3];
-fourthListEl.classList = 'item';
-
-
-
-const fifthListEl = document.createElement('li');
-fifthListEl.textContent = ingredients[4];
-fifthListEl.classList = 'item';
-
-
-
-const sixthListEl = document.createElement('li');
-sixthListEl.textContent = ingredients[5];
-sixthListEl.classList = 'item';
-
+let a = ingredients.map(item => {
+  const b = document.createElement('li');
+  b.classList = 'item';
+  b.textContent = item;
+  console.log(b)
 
 const ingredientsUlEl = document.querySelector('#ingredients');
-ingredientsUlEl.append(firstListEl, secondListEl, thirdListEl, fourthListEl, fifthListEl, sixthListEl);
+ingredientsUlEl.append(b);
+});
 
-console.log(ingredientsUlEl);
