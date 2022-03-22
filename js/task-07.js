3b1273626e8b9eb484108b3e75e3307f78217665
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-22 19:26:27 +0200

    Task 7 Update NEW

--- js/task-07.js
+++ js/task-07.js
@@ -2,9 +2,9 @@
 const inputEl = document.querySelector('#font-size-control');
 const spanEl = document.querySelector('#text');
 
-inputEl.addEventListener('input', textLengh)
-
-
-function textLengh(){
-    spanEl.style.fontSize = inputEl.value + 'px';
+inputEl.addEventListener('input', function textLengh(){
+    spanEl.style.fontSize = inputEl.value + 'pt';
     }
+)
+
+
