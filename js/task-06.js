ef55d8c8431c5389ee705deedc21ef12f2967449
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-22 19:15:57 +0200

    Task 6 Update NEW

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,21 @@
 
 
 const inputEl = document.querySelector('#validation-input');
+const inputElLengh = inputEl.dataset.length;
 
 inputEl.addEventListener('blur', textLengh);
 
 
 function textLengh(event){
-    if(event.currentTarget.value.length !== 6){
-        inputEl.classList.add('invalid');
-    } else if(event.currentTarget.value.length === 6) {
+    if(event.currentTarget.value.length === 6){      // <----------- Я не розумію, чому 
+                                                    // коли я замість 6 ставлю inputElLengh 
+                                                    // воно не працює, а внизу де є !== працює????
+
         inputEl.classList.replace('invalid', 'valid');
+    } else if(event.currentTarget.value.length !== inputElLengh) {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
     }
 }
+
+
